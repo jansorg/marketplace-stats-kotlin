218e15735a234637a155841656ea2c581792a882
Joachim Ansorg <github@ja-dev.eu>
2023-09-06 10:17:04 +0200

    chore: add deployment script

---
+++ deploy.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Joachim Ansorg.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+
+set -e -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+VERSION="$(cat "$DIR/VERSION.txt")"
+echo "Building Version $VERSION..."
+
+./gradlew clean build
+
+git tag --force "v$VERSION"
+git push --tags
+gh release create "v$VERSION" --generate-notes ./build/libs/marketplace-*.jar
+
+docker build -f Dockerfile -t "jansorg/jetbrains-marketplace-stats:$VERSION" .
+docker tag "jansorg/jetbrains-marketplace-stats:$VERSION" "jansorg/jetbrains-marketplace-stats:latest"
+docker push jansorg/jetbrains-marketplace-stats
+
