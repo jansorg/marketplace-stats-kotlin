5641ef283708fc8cf54fd4d91c91148b1539c7a1
Joachim Ansorg <github@ja-dev.eu>
2024-07-05 13:29:30 +0200

    fix: don't show type of trial for "Trials Today". Organizations don't seem to start trials

--- marketplace-data/src/main/kotlin/dev/ja/marketplace/data/trialsToday/TrialsTodayTable.kt
+++ marketplace-data/src/main/kotlin/dev/ja/marketplace/data/trialsToday/TrialsTodayTable.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Joachim Ansorg.
+ * Copyright (c) 2023-2024 Joachim Ansorg.
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
@@ -11,14 +11,14 @@ import dev.ja.marketplace.client.YearMonthDay
 import dev.ja.marketplace.data.*
 import java.math.BigInteger
 
-class TrialsTodayTable : SimpleDataTable("Trials Today", cssClass = "small table-striped"), MarketplaceDataSink {
-    private val columnType = DataTableColumn("type", null, "col-right")
+class TrialsTodayTable(private val now: YearMonthDay = YearMonthDay.now()) :
+    SimpleDataTable("Trials Today", cssClass = "small table-striped"), MarketplaceDataSink {
+    private lateinit var trials: List<PluginTrial>
+
     private val columnCountry = DataTableColumn("country", null, "col-right")
     private val columnCount = DataTableColumn("count", null, "num")
-    override val columns: List<DataTableColumn> = listOf(columnType, columnCountry, columnCount)
 
-    private val now = YearMonthDay.now()
-    private lateinit var trials: List<PluginTrial>
+    override val columns: List<DataTableColumn> = listOf(columnCountry, columnCount)
 
     override fun init(data: PluginData) {
         trials = data.trials
@@ -35,10 +35,9 @@ class TrialsTodayTable : SimpleDataTable("Trials Today", cssClass = "small table
         val trialRows = trials
             .groupBy { it.customer.type }
             .mapValues { it.value.groupBy { it.customer.country } }
-            .flatMap { (type, countryTrials) ->
+            .flatMap { (_, countryTrials) ->
                 countryTrials.map { (country, trials) ->
                     SimpleDateTableRow(
-                        columnType to type,
                         columnCountry to (country.takeIf(Country::isNotEmpty) ?: "—"),
                         columnCount to trials.size.toBigInteger()
                     )
