1ca82e3ba25fd046953e978d18a3f046ab04d165
Joachim Ansorg <github@ja-dev.eu>
2024-07-12 19:14:59 +0200

    chore: formatting amounts

--- marketplace-data/src/main/kotlin/dev/ja/marketplace/data/format/Formatters.kt
+++ marketplace-data/src/main/kotlin/dev/ja/marketplace/data/format/Formatters.kt
@@ -5,9 +5,32 @@
 
 package dev.ja.marketplace.data.format
 
+import org.javamoney.moneta.format.AmountFormatParams
+import org.javamoney.moneta.format.CurrencyStyle
 import java.util.*
+import javax.money.format.AmountFormatQueryBuilder
+import javax.money.format.MonetaryAmountFormat
 import javax.money.format.MonetaryFormats
 
 object Formatters {
-    val MonetaryAmount = MonetaryFormats.getAmountFormat(Locale.getDefault())
+    val MonetaryAmount: MonetaryAmountFormat = createMoneyFormatter(Locale.getDefault())
+
+    private fun createMoneyFormatter(locale: Locale): MonetaryAmountFormat {
+        return when {
+            // by default, the format is like "USD1,000.23" without a space, which isn't readable
+            locale.language == "en" -> MonetaryFormats.getAmountFormat(
+                AmountFormatQueryBuilder
+                    .of(locale)
+                    .set(AmountFormatParams.PATTERN, "###,###.## ¤")
+                    .build()
+            )
+
+            else -> MonetaryFormats.getAmountFormat(
+                AmountFormatQueryBuilder
+                    .of(locale)
+                    .set(CurrencyStyle.CODE)
+                    .build()
+            )
+        }
+    }
 }
