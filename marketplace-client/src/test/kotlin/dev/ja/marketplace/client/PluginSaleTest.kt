19674d2cd4fde6c7f52cfe758c27e27ee2379cf8
Joachim Ansorg <github@ja-dev.eu>
2025-02-11 21:02:04 +0100

    Better fix for sales of perpetual licenses

--- marketplace-client/src/test/kotlin/dev/ja/marketplace/client/PluginSaleTest.kt
+++ marketplace-client/src/test/kotlin/dev/ja/marketplace/client/PluginSaleTest.kt
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2024 Joachim Ansorg.
+ * Copyright (c) 2024-2025 Joachim Ansorg.
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package dev.ja.marketplace.client
 
+import dev.ja.marketplace.client.model.LicensePeriod
 import dev.ja.marketplace.client.model.PluginSale
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -60,5 +61,57 @@ class PluginSaleTest {
 
         val sale = Json.decodeFromString<PluginSale>(json)
         assertEquals("EUR 3.9", sale.lineItems[0].amount.toString())
+    }
+
+    @Test
+    fun `nullable license period`() {
+        val json = """
+        {
+            "ref": "my-ref",
+            "date": [
+                2024,
+                6,
+                12
+            ],
+            "amount": 3.90,
+            "amountUSD": 4.19,
+            "currency": "EUR",
+            "period": null,
+            "customer": {
+                "code": 42,
+                "name": "",
+                "country": "Germany",
+                "type": "Personal"
+            },
+            "reseller": null,
+            "lineItems": [
+                {
+                    "type": "NEW",
+                    "licenseIds": [
+                        "abcdef"
+                    ],
+                    "subscriptionDates": {
+                        "start": [
+                            2024,
+                            6,
+                            12
+                        ],
+                        "end": [
+                            2024,
+                            7,
+                            11
+                        ]
+                    },
+                    "amount": 3.90,
+                    "amountUsd": 4.19,
+                    "discountDescriptions": []
+                }
+            ]
+        }            
+        """.trimIndent()
+
+        val sale = Json.decodeFromString<PluginSale>(json)
+        assertEquals("EUR 3.9", sale.lineItems[0].amount.toString())
+        assertEquals(LicensePeriod.Perpetual, sale.licensePeriod)
     }
 }
